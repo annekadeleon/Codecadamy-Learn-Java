85704faf02766389e3c7f8c3b70c42a37bc935a2
annekadeleon <annsophdl@gmail.com>
2017-07-15 17:24:31 +0800

    Create Generalizations.java

---
+++ Generalizations.java
@@ -0,0 +1,15 @@
+public class Generalizations {
+	public static void main(String[] args) {
+
+		//this is a single line comment
+    boolean isComplete;
+    isComplete = true;
+		int awesomeLevel;
+    awesomeLevel = 121;
+		int epicLevel;
+    epicLevel = awesomeLevel * 2;
+		System.out.println(epicLevel);
+		
+
+	}
+}
