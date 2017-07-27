37c4fd1f637e750fc49da9b0f43edba1d4fe0691
Anneka de Leon <annsophdl@gmail.com>
2017-07-27 19:55:28 +0800

    Update Temperatures.java

--- Temperatures.java
+++ Temperatures.java
@@ -6,6 +6,13 @@ public class Temperatures {
     
     ArrayList<Integer> weeklyTemperatures = new
     ArrayList<Integer>();
+    weeklyTemperatures.add(78);
+    weeklyTemperatures.add(67);
+    weeklyTemperatures.add(89);
+    weeklyTemperatures.add(94);
+    
+    System.out.println( weeklyTemperatures.get(1) );
+    
 
 
 	}
